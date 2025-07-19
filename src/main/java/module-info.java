6d09497cd38dcab1fc2df7d303526a148540acdb
Eduardo Garcia <markesiano@live.com.mx>
2025-07-19 14:49:35 -0600

    Updated: modified the module-info for export interfaces, constants and support clases

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.markesiano.markdi {
+    exports com.markesiano.serviceprovider;
+    exports com.markesiano.serviceprovider.interfaces;
+    exports com.markesiano.serviceprovider.constants;
+    exports com.markesiano.serviceprovider.support;
+    requires java.logging;
+}
